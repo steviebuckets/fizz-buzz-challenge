37ee4be922ced0e2bed31e34fc3c70957a0ca744
Stevie Buckets <steviebuckets33@gmail.com>
2016-04-26 15:46:50 -0700

    modified index.html and app.js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,42 @@
+function fizz() {
 
-
-for (var i = 1; i <= 100; i ++) 
-{
-
-	if (i % 15 === 0){
-	console.log("FizzBuzz");
-	document.write("FizzBuzz");
-	document.write("<br/>");
-}
+	function write(string){
+		$('#output').append(string + '<br/>')
+	}
 	
-	else if (i % 5 === 0){
-	console.log("Buzz");
-  document.write("Buzz");
-	document.write("<br/>");
+var number = parseInt(document.getElementById("number").value);
 
-}
-	else if (i % 3 === 0){
-	console.log("Fizz");
-  document.write("Fizz");
-	document.write("<br/>");
+if (!number){
+	alert("Input a number");
 }
 
+for (var i = number; i<=100; i++) {
 
-else {
-console.log(i);
-document.write(i);
-document.write("<br/>");
+if (i % 15 === 0) {
+  write('FizzBuzz')
+// document.getElementById("output").write("FizzBuzz");
+// document.write("<br/>");
 }
 
+else if (i % 3 === 0){
+  write('Fizz')
+// document.write("Fizz");
+// document.write("<br/>");
 }
 
 
+else if (i % 5 === 0) {
+	write('Buzz')
+ // document.write("Buzz");
+ // document.write("<br/>");
+}
 
 
+else{
+	write(i)
+// document.write(i);
+// document.write("<br/>");
+}
 
-
-
-
-
-
-
+ }
+}
